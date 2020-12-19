0e991fa264e9b3548add726e05e932b3f7a5f783
elid venega <venegaelid@gmail.com>
2020-12-19 11:18:21 -0800

    js objects update

---
+++ objects2.js
@@ -0,0 +1,18 @@
+// Math object
+
+console.log(Math);
+
+
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.random(random * 100));
